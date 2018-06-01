81c051d4aa3dd49feef4d9f826c698f78ab2bc3f
Hussein Mohammed <zidhussein@gmail.com>
2018-06-01 19:43:13 +0100

    [022] fix js

--- solutions/022/022.js
+++ solutions/022/022.js
@@ -1,11 +1,17 @@
 const fs = require('fs')
 
-const names = fs.readFileSync('./resources/names.txt').toString().split(',')
-  .map(x => x.replace(/\"/g, '')).sort()
+const names = fs.readFileSync('./resources/names.txt')
+  .toString()
+  .replace('\n', '')
+  .split(',')
+  .map(x => x.replace(/\"/g, ''))
+  .sort()
 
 let sum = 0
-for (let i = 0; i < names.length; i++) {
-  sum += names[i].split('').map(x => x.charCodeAt(0) - 64)
+for (let i = 0; i < names.length; i++)
+  sum += names[i]
+    .split('')
+    .map(x => x.charCodeAt(0) - 64)
     .reduce((a, b) => a + b) * (i + 1)
-}
+
 console.log(sum)
